610ac399a3252e42c42c7d51112e4e41523b17fe
Harsh Verma <hv.harshverma025@gmail.com>
2025-06-15 17:58:00 +0530

    Day 4

---
+++ 02. Array/Q8.Sort0and1.cpp
@@ -0,0 +1,17 @@
+// // User function template for C++
+
+// class Solution {
+//   public:
+//     void segregate0and1(vector<int> &arr) {
+//         // code here
+//         int n = arr.size();
+        
+//         int idx = 0;
+//         for(int i = 0; i<n; i++){
+//             if(arr[i] == 0){
+//                 swap(arr[i], arr[idx]);
+//                 idx++;
+//             }
+//         }
+//     }
+// };
