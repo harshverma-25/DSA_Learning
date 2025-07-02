20b0436a4c897085a66f5251befeabcfdc53f12c
Harsh Verma <hv.harshverma025@gmail.com>
2025-07-02 22:11:07 +0530

    Day 21

--- 06.Searching/08.CapacitytoshipPackages.cpp
+++ 06.Searching/08.CapacitytoshipPackages.cpp
@@ -1,34 +1,34 @@
-class Solution {
-public:
-    bool canShip(vector<int>& weights, int days, int capacity) {
-        int currWeight = 0;
-        int requiredDays = 1;
+// class Solution {
+// public:
+//     bool canShip(vector<int>& weights, int days, int capacity) {
+//         int currWeight = 0;
+//         int requiredDays = 1;
 
-        for (int i = 0; i < weights.size(); i++) {
-            if (currWeight + weights[i] > capacity) {
-                requiredDays++;
-                currWeight = 0;
-            }
-            currWeight += weights[i];
-        }
+//         for (int i = 0; i < weights.size(); i++) {
+//             if (currWeight + weights[i] > capacity) {
+//                 requiredDays++;
+//                 currWeight = 0;
+//             }
+//             currWeight += weights[i];
+//         }
 
-        return requiredDays <= days;
-    }
+//         return requiredDays <= days;
+//     }
 
-    int shipWithinDays(vector<int>& weights, int days) {
-        int low = *max_element(weights.begin(), weights.end());  
-        int high = accumulate(weights.begin(), weights.end(), 0); 
+//     int shipWithinDays(vector<int>& weights, int days) {
+//         int low = *max_element(weights.begin(), weights.end());  
+//         int high = accumulate(weights.begin(), weights.end(), 0); 
 
-        while (low < high) {
-            int mid = low + (high - low) / 2;
+//         while (low < high) {
+//             int mid = low + (high - low) / 2;
 
-            if (canShip(weights, days, mid)) {
-                high = mid; 
-            } else {
-                low = mid + 1; 
-            }
-        }
+//             if (canShip(weights, days, mid)) {
+//                 high = mid; 
+//             } else {
+//                 low = mid + 1; 
+//             }
+//         }
 
-        return low;
-    }
-};
+//         return low;
+//     }
+// };
